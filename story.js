8d3a78ab1651bb53d2d5b8a07031b04696570887
Meshari Aldoukhi <mashry151@hotmail.com>
2021-05-24 16:53:21 +0300

    Created new file

--- story.js
+++ story.js
@@ -1,9 +1,7 @@
 // decleare Varibles 
-
 let name = "Laila";
 let age = 25;
 let hobby = "Hoarding Books";
 
 //console result
-console.log(name + " in 33 years, you will be " + (age + 33) + " years old. By then you'll have decades of "+ hobby + " practice!");
 console.log(`${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`);
