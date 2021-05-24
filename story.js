f129d58eec247ee9c4d1668cd84cdbedd9b3b4bb
Meshari Aldoukhi <mashry151@hotmail.com>
2021-05-24 16:34:17 +0300

    Created new file

--- story.js
+++ story.js
@@ -3,10 +3,11 @@
     let name, 
         age,
         hobby;
-        
+
 name = "Laila";
 age = 25;
 hobby = "Hoarding Books";
 
+//console result
 console.log(name + " in " + age + " years, you will be " + (age + 33) + " years old. By then you'll have decades of "+ hobby + " practice!");
 console.log(`${name}, in ${age} years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`);
